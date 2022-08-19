8ee48c4b6c670180d99c80d91cdd015be9cdc910
unknown <samuel.washington@snhu.edu>
2022-08-18 19:07:43 -0600

    Updated Pipfile + Changed Database Setting + Created Music App

---
+++ music_library_project/local_setting.py
@@ -0,0 +1,14 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-_)@)yh%zh**kjxy#3@$x34^o9@1#_c2=0xv80or9=f9l^u-k-7'
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'music_library_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'ARCHeesecake$$08'
+    }
+}
--- music_library_project/settings.py
+++ music_library_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_)@)yh%zh**kjxy#3@$x34^o9@1#_c2=0xv80or9=f9l^u-k-7'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'music_library_project.urls'
@@ -68,18 +67,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'music_library_project.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -121,3 +108,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try: 
+    from music_library_project.local_setting import *
+except ImportError:
+    pass
+
+CORS_ORIGIN_ALLOW_ALL=True
